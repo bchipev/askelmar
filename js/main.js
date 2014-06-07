bec97d2d7a1337a967b6b98a04c688dfac2ca655
bchipev <bozhan@Bozhans-MacBook-Pro.local>
2014-06-07 17:00:06 +0200

    Added a couple of new sentences.

--- js/main.js
+++ js/main.js
@@ -1,9 +1,15 @@
 $(function() {
     var sentences = [
-        "Let me put on my slippers",
-        "Sentence Two Bla Bla",
-        "Sentence Three Bla Bla",
-        "Sentence Four Bla Bla"
+        "Let me put on my slippers first.",
+        "I don't want to sit next to Dominik.",
+        "Skype me a Hipchat message on my e-mail.",
+        "Sorry, reinstalling Windows. Try again in 8 hours.",
+        "Try Google.",
+        "Are you still chewing? Let me know when your mouth is ready.",
+        "Are you sure your computer is plugged in?",
+        "Boot in safe mode.",
+        "Call the care police.",
+        "I can grill some meat."
     ];
     var getRandomInteger = function(min, max) {
         return Math.floor((Math.random() * max) + min);
